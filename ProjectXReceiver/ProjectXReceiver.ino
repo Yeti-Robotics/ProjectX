7b1b2bd9f0d316089f140dcdda1bdc3541a99dcd
wazateer1 <wazateer1@gmail.com>
2016-04-12 07:32:47 -0400

    - You have to init the motor driver shield

--- ProjectXReceiver/ProjectXReceiver.ino
+++ ProjectXReceiver/ProjectXReceiver.ino
@@ -1,12 +1,10 @@
-#include <Dhcp.h>
-#include <Dns.h>
 #include <Ethernet.h>
 #include <EthernetClient.h>
 #include <EthernetServer.h>
 #include <EthernetUdp.h>
 #include <DualVNH5019MotorShield.h>
 
-/90-a2-da-0f-25-E7
+//90-a2-da-0f-25-E7
 byte mac[] = {0x90, 0xA2, 0xDA, 0x0f, 0x25, 0xE7};
 
 //ip Address for shield
@@ -15,6 +13,8 @@ byte ip[] = {192,168,1,113};
 //Use port 23 for telnet
 EthernetServer server = EthernetServer(23);
 
+//Motor shield
+DualVNH5019MotorShield md;
 
 void setup() {
 	// put your setup code here, to run once:
@@ -28,16 +28,20 @@ void setup() {
 	if(server.available()){
 		Serial.println("Client available");
 	}
+
+  md.init();
 }
 
 void loop() {
 	EthernetClient client = server.available();
-	message = client.read();
-	Serial.println("Client Message" + message);
-	Serial.println(message);
-	if(message & 128) {
-		setM1Speed(message & 127);
-	} else {
-		setM2Speed(message & 127);
+	if(client) {
+	  char message = client.read();
+    Serial.println("Client Message" + message);
+    Serial.println(message);
+    if(message & 128) {
+      md.setM1Speed(message & 127);
+    } else {
+      md.setM2Speed(message & 127);
+    }
 	}
 }
